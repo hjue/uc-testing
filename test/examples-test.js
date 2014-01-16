8e34ac0ae006a4d2b131bc30f239d34c721c6116
hjue <haojue@gmail.com>
2014-01-16 17:11:07 +0800

    add http server for testing

--- test/examples-test.js
+++ test/examples-test.js
@@ -1,20 +1,23 @@
 var APIeasy = require('api-easy'),
-  assert = require('assert');;
+  assert = require('assert');
+
 var suite = APIeasy.describe('api');
 
 suite.discuss('Test Post Json String')     
-    .use('local.my.csdn.net', 80)
+    .use('127.0.0.1', 8000)
     .setHeader('Content-Type', 'application/json')
-    .post('/test.php', {a:1})
+    .post('/tests', {a:1})
       .expect(200, {a:1})
       
 suite.discuss("Test Post Key-Value")
-    .use('local.my.csdn.net', 80)
+    .use('127.0.0.1', 8000)
     .setHeader('Content-Type', 'application/x-www-form-urlencoded')    
-    .post('/test.php', {a:1})
+    .post('/tests', {a:1})
       .expect(200, {a:1})    
       .expect('check body', function (err, res, body) {
           assert.include(body, 'a');
       })
 
 suite.export(module);
+
+
